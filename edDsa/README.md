f9476a82a73545b2d6d5b2167ef24b1ec5f69c67
Sumit <111978673+sumitk-jma@users.noreply.github.comsumitk.jma@gmai.com>
2024-09-11 04:51:24 +0530

    add edDSA

---
+++ edDsa/README.md
@@ -0,0 +1,8 @@
+EdDSA - Edwards-curve Digital Signature Algorithm  - ED25519
+
+Using @noble/ed25519 NPM Library
+
+Generating a private key.
+Generating public key using that.
+Signing a random message using that privatekey.
+Verifying it using the publiuc key.
---
+++ edDsa/src/index.ts
@@ -0,0 +1,26 @@
+import * as ed from "@noble/ed25519";
+
+async function main() {
+  // Generate a secure random private key
+  const privKey = ed.utils.randomPrivateKey();
+
+  // Convert the message "hello world" to a Uint8Array
+  const message = new TextEncoder().encode("Sumit K");
+
+  // Generate the public key from the private key
+  const pubKey = await ed.getPublicKeyAsync(privKey);
+
+  // Sign the message
+  const signature = await ed.signAsync(message, privKey);
+
+  // Verify the signature
+  const isValid = await ed.verifyAsync(signature, message, pubKey);
+
+  // Output the result
+  console.log(pubKey);
+  console.log(signature);
+  console.log(privKey);
+  console.log(isValid); // Should print `true` if the signature is valid
+}
+
+main();
