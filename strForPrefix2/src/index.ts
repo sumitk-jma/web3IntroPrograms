cf7c08ac79ccc822242816d19b4cce43025dd6e6
Sumit <111978673+sumitk-jma@users.noreply.github.comsumitk.jma@gmai.com>
2024-09-11 00:55:05 +0530

    add strWIthPrefix2

---
+++ strForPrefix2/src/index.ts
@@ -0,0 +1,27 @@
+import * as crypto from 'crypto';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the prefix to start hash with: ', (prefix: string) => {
+  let input = 0;
+
+  while (true) {
+    let inputStr = `Sumit => jayant | Rs 100
+Khushi => Megha | Rs 10` + input.toString();
+    let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+
+    if (hash.startsWith(prefix)) {
+      
+      console.log(`Desired String: ${inputStr}`);
+      console.log(`Hash: ${hash}`);
+      rl.close();
+      return;
+    }
+
+    input++;
+  }
+});
